411a01ff5ac8a94b28777ab056adf0cdbb3e2f9f
waghshivam322@gmail.com <waghshivam322@gmail.com>
2025-07-27 12:36:04 +0530

    updated

--- Basic Problems/NumberSwap.java
+++ Basic Problems/NumberSwap.java
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 public class NumberSwap {
     
     public static void main(String[] args) {
@@ -13,19 +12,3 @@ public class NumberSwap {
         System.out.println("a="+a+" b="+b);//after swapping
     }
 }
-=======
-public class NumberSwap {
-    
-    public static void main(String[] args) {
-        
-        int a=2; int b=3;
-        System.out.println("a="+a+" b="+b);//before swapping
-
-        int temp;
-        temp=a;
-        a=b;
-        b=temp;
-        System.out.println("a="+a+" b="+b);//after swapping
-    }
-}
->>>>>>> Stashed changes
--- Basic Problems/NumberSwapUsingArithmetic.java
+++ Basic Problems/NumberSwapUsingArithmetic.java
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 public class NumberSwapUsingArithmetic {
      public static void main(String[] args) {
     int a=2; int b=3;
@@ -11,17 +10,3 @@ System.out.println("a="+a+" b="+b);
    }
 
 }
-=======
-public class NumberSwapUsingArithmetic {
-     public static void main(String[] args) {
-    int a=2; int b=3;
-   System.out.println("a="+a+" b="+b);//before swap
-   a=a+b;
-   b=a-b;
-   b=a-b;
-System.out.println("a="+a+" b="+b);
-     
-   }
-
-}
->>>>>>> Stashed changes
