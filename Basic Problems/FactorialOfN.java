a3ea54ec2dadea827c71a18fe8513d3fa947cadc
waghshivam322@gmail.com <waghshivam322@gmail.com>
2025-08-01 09:01:26 +0530

    updated some problems

--- Basic Problems/FactorialOfN.java
+++ Basic Problems/FactorialOfN.java
@@ -3,13 +3,14 @@ public class FactorialOfN {
     public static int factorial(int n)
     {
        int res=1;
-       for(int i=2;i<n;i++)
+       for(int i=2;i<=n;i++)
        {
-        res*=i;//i.e res=res+i;
+        res*=i;//i.e res=res+i; add factorial such as 5!=5*4*3*2*1=120
        }
        return res;
     }
-    public static void main(String[] args) {
+    public static void main(S
+    tring[] args) {
         int res=6;
         System.out.println("factorial of the given number is = "+factorial(res));
     }
