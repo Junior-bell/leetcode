9b100409948c5f7efeaebe95d93c6ec929fb8c77
waghshivam322@gmail.com <waghshivam322@gmail.com>
2025-08-04 07:30:32 +0530

    revised some problems

--- PluseOne.java
+++ PluseOne.java
@@ -1,19 +1,22 @@
+
+//leetcode's plus one problem
 public class PluseOne {
     
     public int[] pluseone(int[] digits)
     {
-        for(int i=digits.length-1;i>0;i--)
+        //we have to loop through array to add a number in the end
+        for(int i=digits.length-1;i>0;i--)//start the loop from end of the loop
         {
-            if(digits[i]<9)
+            if(digits[i]<9)//if the array number is smaller than 9
             {
-                digits[i]++;
-                return digits;
+                digits[i]++;  //increase the numnber count 
+                return digits; //and return 
             }
-             digits[i]=0;
+             digits[i]=0;//if the number is 9 then return 0 instead of 9 
         }
-        int[] newNumber= new int[digits.length+1];
-        newNumber[0]=1;
-        return newNumber;
+        int[] newNumber= new int[digits.length+1];//create a newNumber array
+        newNumber[0]=1;//put 1 as the first element 
+        return newNumber;//return the array.
     }
     public static void main(String[] args) {
         PluseOne solution = new PluseOne();
