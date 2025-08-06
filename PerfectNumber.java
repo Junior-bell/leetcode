edec0ca22bd117dc65089ce215f4598b3f0482d0
waghshivam322@gmail.com <waghshivam322@gmail.com>
2025-08-06 09:31:47 +0530

    solved a program to find the perfect number

---
+++ PerfectNumber.java
@@ -0,0 +1,27 @@
+
+//program to find the perfect number 
+public class PerfectNumber {
+
+    public static boolean perfect(int a)
+    {
+        int sum=0;
+         for(int i=1;i<=a/2;i++)
+         {
+            if(a%i==0){ 
+            System.out.print(i+" ");
+             sum+=i;
+            }
+         }
+        System.err.println();
+    return sum==a;     //is sum and the given numbers are the same? yes or no?
+    }
+    
+    public static void main(String[] args) {
+        int a=28;
+        if(perfect(a))
+        System.out.println("The given number is a perfect number ");
+        else
+        System.out.println("The given number is not a perfect Number ");
+    }
+    
+}
